bb66575b41d2169e9109f901d30f57c51eebbda4
athulkrishnapulari-sudo <athulkrishnapulari@gmail.com>
2026-07-08 21:59:26 +0530

    Add GitHub Pages workflow

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'brew-haven-react'
-
 export default defineConfig({
-  base: process.env.BASE_PATH || `/${repoName}/`,
+  base: '/brew-haven-react/',
   plugins: [react()],
 })
